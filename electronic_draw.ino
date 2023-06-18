9fa32a954a1564cb222a7f24999ee0a1d93224c0
Looading99 <113303743+Looading99@users.noreply.github.com>
2023-06-18 20:33:09 +0800

    Update electronic_draw.ino

--- electronic_draw.ino
+++ electronic_draw.ino
@@ -14,8 +14,8 @@
 #define LEN_CALLED 31
 #define LEN_BLACKLIST sizeof(blacklist)
 
-uint8_t blacklist[] = {46}, use = 0, called_last_modified = LEN_CALLED - 1;
-uint32_t last_use_sec = 0;
+uint8_t blacklist[] = {46}, use = 0, called_last_modified = LEN_CALLED-1;
+uint32_t last_use = 0;
 
 Energy energy;
 LedControl max7219 = LedControl(MAX7219_DIN, MAX7219_CLK, MAX7219_CS);
@@ -31,17 +31,20 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(2), touch_interrupt, RISING);
   ssd1306_128x64_i2c_init();
   ssd1306_clearScreen();
-
+  
   randomSeed(analogRead(A0)*analogRead(A2));
-
-  uint8_t mask = EEPROM.read(0), ui;
-  for (ui = 0; ui < NUM_OF_STUDENTS; ui++) {
-    if (EEPROM.read(ui) >> 6 != mask) called_last_modified = ui;
+  
+  uint8_t mask = EEPROM.read(0) >> 6, ui;
+  for (ui = 0; ui < LEN_CALLED; ui++) {
+    if ((EEPROM.read(ui) >> 6) != mask) {
+      called_last_modified = ui;
+      break;
+    }
   }
 }
 
 void loop() {
-  if ((millis()-last_use_sec)>30*1000) {
+  if ((millis()-last_use)>30*1000) {
     max7219.shutdown(0, true);
     ssd1306_clearScreen();
     energy.PowerDown();
@@ -61,7 +64,7 @@ void loop() {
   }
   called_last_modified = (called_last_modified + 1) % LEN_CALLED;
   id = get_random_id();
-  EEPROM.write(called_last_modified, id | ((EEPROM.read(called_last_modified - 1) >> 6) + 1) % 0b11);
+  EEPROM.write(called_last_modified, id | ((EEPROM.read(called_last_modified)>>6)+1)%4<<6);
   for (ui = 0; ui < 2; ui++) {
     show_id(id);
     delay(200);
@@ -71,7 +74,7 @@ void loop() {
   }
   show_id(id);
 
-  last_use_sec = millis();
+  last_use = millis();
   use = 0;
 }
 
@@ -80,7 +83,7 @@ void touch_interrupt(void) {
 }
 
 void show_id(uint8_t id) {
-  uint8_t a = id / 10, b = id % 10, r = 0;
+  uint8_t a = id/10, b = id%10, r = 0;
   for (; r<4; r++) max7219.setRow(0, r, pgm_read_byte(&digits_bitmap[a][r]));
   for (; r<8; r++) max7219.setRow(0, r, pgm_read_byte(&digits_bitmap[b][r-4]));
   ssd1306_drawBitmap(0, 2, 32*3, 32, names_bitmap[id-1]);
@@ -90,7 +93,7 @@ uint8_t get_random_id(void) {
   uint8_t id, ui;
   gen_id:
   id = random(1, NUM_OF_STUDENTS+1);
-  for (ui = 0; ui < LEN_CALLED; ui++) if (id == EEPROM.read(ui) & 0b00111111) goto gen_id;
+  for (ui = 0; ui < LEN_CALLED; ui++) if (id == (EEPROM.read(ui)&0b00111111)) goto gen_id;
   for (ui = 0; ui < LEN_BLACKLIST; ui++) if (id == blacklist[ui]) goto gen_id;
   return id;
 }
